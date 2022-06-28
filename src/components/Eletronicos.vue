06c0cb81c37f2b478c2194e7a80162d9997ac474
Jessica Monteiro Bueno <monteirojeh42@gmail.com>
2022-06-27 22:52:54 -0300

    Inclusão de API

---
+++ src/components/Eletronicos.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="container">
+    <h1>PRODUTOS EM DESTAQUE</h1>
+
+    <ul>
+      <li v-for="product in array_products" :key="product.id">
+        <img :src="product.image" alt="">
+        {{ product.title }}
+        {{ product.price }}
+        
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  name: "Produtos",
+  data() {
+    return {
+      array_products: []
+    }
+  },
+  methods: {
+    async loadProducts() {
+
+      axios.get('https://fakestoreapi.com/products/category/eletronics')
+      .then( (response) => {
+        this.array_products = response.data
+        console.log(this.array_products)
+      })
+
+      .catch( (erro) => {
+        console.log(erro)
+      })
+    }
+  },
+  created() {
+    this.loadProducts()
+  }
+}
+</script>
+
+
+<style scoped>
+#container {
+  height: 150vh;
+  width: 80vw;
+  border: 2px solid black;
+  margin: 20px auto;
+  
+}
+#container h1 {
+  color: #4b4848;
+  text-align: center;
+  padding-top: 15px;
+}
+#container li{
+  text-decoration: none;
+}
+#container li img{
+  width: 150px;
+  
+}
+</style>
--- src/components/Produtos.vue
+++ src/components/Produtos.vue
@@ -1,28 +1,96 @@
 <template>
-  <div id="container">
+  <section id="container">
     <h1>PRODUTOS EM DESTAQUE</h1>
-  </div>
+    <div class="shopping">
+      <ul>
+        <li v-for="product in array_products" :key="product.id">
+          <img :src="product.image" alt="" />
+          {{ product.title }} <br>
+          {{ product.price }}
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Produtos",
+  data() {
+    return {
+      array_products: [],
+    };
+  },
+  methods: {
+    async loadProducts() {
+      axios
+        .get("https://fakestoreapi.com/products")
+        .then((response) => {
+          this.array_products = response.data;
+          console.log(this.array_products);
+        })
+
+        .catch((erro) => {
+          console.log(erro);
+        });
+    },
+  },
+  created() {
+    this.loadProducts();
+  },
 };
 </script>
 
-<script>
-</script>
 
 <style scoped>
+
 #container {
-  height: 150vh;
-  width: 80vw;
-  border: 2px solid black;
-  margin: 20px auto;
+  text-align: center;
+  justify-content: center;
+  align-items: center; 
+  width: 80%;
+  margin-left: 10%;
+  
+  
 }
 #container h1 {
   color: #4b4848;
   text-align: center;
-  padding-top: 15px;
+  padding: 30px;
+  font-size: 30px;
 }
+#container ul li {
+  border: 2px solid rgba(128, 128, 128, 0.349);
+  width: 300px;
+  height: 400px;
+  display: block;
+  gap: 1rem;
+  border-radius: 20px;
+  cursor: pointer;
+}
+#container ul {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 50px;
+}
+#container li{
+  padding: 10px;
+  box-shadow: 2px 2px rgba(128, 128, 128, 0.363);
+}
+#container li img {
+  width: 110px;
+  padding-top: 20px;
+  display: block;
+ 
+}
+#container img {
+  margin: auto;
+  padding-bottom: 20px;
+}
+
 </style>
