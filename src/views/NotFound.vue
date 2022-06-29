37738733cb5f5f55ee704b0acaf8069c3f2e282b
Jessica Monteiro Bueno <monteirojeh42@gmail.com>
2022-06-28 21:09:08 -0300

    Criação de view NotFound (página não encontrada)

---
+++ src/views/NotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Página não encontrada</h1>
+     <img src="img/error.gif">
+  </div>
+ 
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+
+</script>
+
+<style>
+h1{
+    text-align: center;
+    font-size: 35px;
+    font-weight: 700;
+    margin-top: 60px;
+    color: #ff1a73;
+}
+img{
+   margin-left: 36%;
+
+}
+</style>
